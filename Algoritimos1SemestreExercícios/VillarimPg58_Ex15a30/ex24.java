8d5e127898d53d7e60fb6f37258e287e014a4180
Luiza <lulurobiattig@gmail.com>
2025-02-14 16:22:05 -0300

    Add ex 24

---
+++ Algoritimos1SemestreExercícios/VillarimPg58_Ex15a30/ex24.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package VillarimPg58_Ex15a30;
+
+/**
+ *
+ * @author Discente
+ */
+public class ex24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
